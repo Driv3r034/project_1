ae501525e8d0d0f04168374d8f9756438a266ea6
Sergei Kolpakov <draiv3r897@gmail.com>
2025-02-08 18:35:34 +0300

    added react router (dom)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import { App } from './App.tsx'
+import { FirstPage } from './pages/first-page/first-page.tsx'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/first" element={<FirstPage />} />
+      </Routes>
+    </Router>
 )
